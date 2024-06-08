79c207b646f6db12c1e64e71680935f940f93b5e
Nick Fedor <71477161+nicholas-fedor@users.noreply.github.com>
2024-06-08 07:59:02 -0700

    Ch2 finished

---
+++ ch2/ex2-4/example.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	_0 := 0.0
+	_1 := 20
+	n := 3
+	ａ := "hello" // Unicode U+FF41
+	fmt.Println(_0)
+	fmt.Println(_1)
+	fmt.Println(n)
+	fmt.Println(ａ)
+}
---
+++ ch2/ex2-5/example.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	ａ := "hello"
+	a := "goodbye"
+	fmt.Println(ａ)
+	fmt.Println(a)
+}
