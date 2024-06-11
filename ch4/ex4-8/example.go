7c062c93bc71d34b6d66cf54a0dff68ce3d5e2f3
Nick Fedor <71477161+nicholas-fedor@users.noreply.github.com>
2024-06-11 05:36:16 -0700

    Finish ex4-8

---
+++ ch4/ex4-8/example.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
+
+// Expected Output:
+// 0
+// 1
+// 2
+// 3
+// 4
+// 5
+// 6
+// 7
+// 8
+// 9
