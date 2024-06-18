d88c3f919d3e4c27013a2164d480d920b67f6bae
Nick Fedor <71477161+nicholas-fedor@users.noreply.github.com>
2024-06-18 10:13:02 -0700

    Finish ex4-16

---
+++ ch4/ex4-16/example.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	samples := []string{"hello", "apple_n!"}
+	for _, sample := range samples {
+		for i, r := range sample {
+			fmt.Println(i, r, string(r))
+		}
+		fmt.Println()
+	}
+}
+
+// Output:
+// 0 104 h
+// 1 101 e
+// 2 108 l
+// 3 108 l
+// 4 111 o
+
+// 0 97 a
+// 1 112 p
+// 2 112 p
+// 3 108 l
+// 4 101 e
+// 5 95 _
+// 6 110 n
+// 7 33 !
